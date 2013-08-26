ee2f641c29c0edb804a34787bcfca008fbeaaa3c
Theo Julienne <theo@icy.com.au>
2013-08-26 12:50:11 +1000

    Import widget to develop branch.

---
+++ widget/options.js
@@ -0,0 +1,8 @@
+return {
+  "name": "LimitlessLED",
+  "sizeX": 1, // Not yet implemented
+  "sizeY": 1, // Not yet implemented
+  "deviceMap": [
+    { "deviceId": [1011, 1012], "minimum": 1, "maximum": 1}
+  ]
+}
