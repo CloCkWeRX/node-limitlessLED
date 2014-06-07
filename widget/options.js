8670f7b180d72daa2cd2fc0191db09f170c746f5
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-06-07 18:37:29 +0930

    Remove ninjablocks UI code

--- widget/options.js
+++
@@ -1,8 +0,0 @@
-return {
-  "name": "LimitlessLED",
-  "sizeX": 1, // Not yet implemented
-  "sizeY": 1, // Not yet implemented
-  "deviceMap": [
-    { "deviceId": [1011, 1012, 1013], "minimum": 1, "maximum": 1}
-  ]
-}
